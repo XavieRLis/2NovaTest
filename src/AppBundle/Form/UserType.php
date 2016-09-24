3a32145c2aaf6dba16ee45e94aff1fde1e8edba7
xavier.lis <xavier.lis@gmail.com>
2016-09-24 13:13:37 +0300

    Create RegistrationForm

---
+++ src/AppBundle/Form/UserType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, array('label' => 'Email'))
+            ->add('username', TextType::class, array('label' => 'Логин'))
+            ->add('plainPassword', RepeatedType::class, array(
+                    'type' => PasswordType::class,
+                    'first_options'  => array('label' => 'Пароль'),
+                    'second_options' => array('label' => 'Повторите пароль'),
+                )
+            );
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'app_user_type';
+    }
+}
