0fa0f8c379ebd617ab38a871bfc6597c6b539528
xavier.lis <xavier.lis@gmail.com>
2016-09-24 13:33:33 +0300

    Add login Action to Security controller

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -43,4 +43,24 @@ class SecurityController extends Controller
             array('form' => $form->createView())
         );
     }
+
+    /**
+     * @Route("/login", name="login")
+     * @return Response
+     */
+    public function loginAction()
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render(':Security:login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
 }
