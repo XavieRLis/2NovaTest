7a390f42821f71dd2e6e0231a76858d12a46120b
xavier.lis <xavier.lis@gmail.com>
2016-09-24 14:00:37 +0300

    Fetch user list in default Controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,10 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $users = $this->getDoctrine()->getRepository('AppBundle:User')->findAll();
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'users' => $users,
         ));
     }
 }
